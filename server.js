17ad2beecfedd10c12ee7cc9a852e209d2449df4
elza-s <elza_somu@yahoo.com>
2021-04-10 13:27:32 -0400

    added files

--- server.js
+++ server.js
@@ -5,5 +5,15 @@ const app = express();
 // Public Folders route
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Load view engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  
+  res.render('index.ejs');
+  }
+);
+
 
 app.listen(process.env.PORT || 5000);
